88e32039140d7bbd73a104f3af3c066c9564c2c4
DavidMayoral <davidmayoral1505@gmail.com>
2023-07-12 22:35:52 +0200

    Arduino code updated

--- Read_load_accelerometer/Read_load_accelerometer.ino
+++ Read_load_accelerometer/Read_load_accelerometer.ino
@@ -1,27 +1,30 @@
-//-------------------------------------------------------------------------------------
-// HX711_ADC.h
-// Arduino master library for HX711 24-Bit Analog-to-Digital Converter for Weigh Scales
-// Olav Kallhovd sept2017
-// Tested with      : HX711 asian module on channel A and YZC-133 3kg load cell
-// Tested with MCU  : Arduino Nano
-//-------------------------------------------------------------------------------------
-
 #include <HX711_ADC.h>
 #if defined(ESP8266)|| defined(ESP32) || defined(AVR)
 #include <EEPROM.h>
 #endif
 
-//pins:
+// PINS
 const int force1dout  = 4; //mcu > HX711 no 1 dout pin
 const int force1sck   = 5; //mcu > HX711 no 1 sck pin
 const int force2dout  = 6; //mcu > HX711 no 2 dout pin
 const int force2sck   = 7; //mcu > HX711 no 2 sck pin
+const int acc1X = A0;
+const int acc1Y = A1;
+const int acc1Z = A2;
+const int acc2X = A3;
+const int acc2Y = A4;
+const int acc2Z = A5;
 
-//HX711 constructor (dout pin, sck pin)
+const int RawMin = 0;
+const int RawMax = 1023;
+
+const int sampleSize = 10;  // size of the measurement sample
+
+//HX711 constructor
 HX711_ADC LoadCell_1(force1dout, force1sck); //HX711 1
 HX711_ADC LoadCell_2(force2dout, force2sck); //HX711 2
 
-// eeprom adress for calibration value (4 bytes)
+// EEPROM adress for calibration value (4 bytes)
 const int calVal_eepromAdress_1 = 0;
 const int calVal_eepromAdress_2 = 4;
 unsigned long t = 0;
@@ -31,6 +34,8 @@ void setup() {
   //Serial.println();
   //Serial.println("Starting...");
 
+  // LOAD CELLS
+  ///////////////////////////////////////////////////////////////
   float calibrationValue_1; // calibration value load cell 1
   float calibrationValue_2; // calibration value load cell 2
 
@@ -46,6 +51,8 @@ void setup() {
   LoadCell_2.begin();
   //LoadCell_1.setReverseOutput();
   //LoadCell_2.setReverseOutput();
+
+  // TARE
   unsigned long stabilizingtime = 4000; // tare preciscion can be improved by adding a few seconds of stabilizing time
   boolean _tare = true; //set this to false if you don't want tare to be performed in the next step
   byte loadcell_1_rdy = 0;
@@ -63,6 +70,18 @@ void setup() {
   LoadCell_1.setCalFactor(calibrationValue_1); // user set calibration value (float)
   LoadCell_2.setCalFactor(calibrationValue_2); // user set calibration value (float)
   //Serial.println("Startup is complete");
+
+
+  // ACCELEROMETERS
+  ///////////////////////////////////////////////////////////////
+
+  // minimum and maximum ranges for each axis
+  const int RawMin = 0;
+  const int RawMax = 1023;
+
+  const int SampleSize = 10;  // size of the measurement sample
+
+  analogReference(EXTERNAL);
 }
 
 void loop() {
@@ -71,21 +90,58 @@ void loop() {
 
   // check for new data/start next conversion:
   if (LoadCell_1.update()) newDataReady = true;
-  LoadCell_2.update();
+  LoadCell_2.update();  
 
-  //get smoothed value from data set
+  
+  //////////////////////////
+  // SERIAL PRINT
+  // get smoothed value from data set
   if ((newDataReady)) {
     if (millis() > t + serialPrintInterval) {
+      // UPDATE LOAD CELLS
       float force1 = LoadCell_1.getData();
       float force2 = LoadCell_2.getData();
+
+      // UPDATE ACCELEROMETERS
+      // read raw values - ReadAxis() takes 10 values and returns the average
+      int xRaw1 = ReadAxis(acc1X);
+      int yRaw1 = ReadAxis(acc1Y);
+      int zRaw1 = ReadAxis(acc1Z);
+      int Raw1[3] = {ReadAxis(acc1X), ReadAxis(acc1Y), ReadAxis(acc1Z)};
+    
+      // converting values to acceleration
+      long xScaled1 = map(xRaw1, RawMin, RawMax, -3000, 3000);
+      long yScaled1 = map(yRaw1, RawMin, RawMax, -3000, 3000);
+      long zScaled1 = map(zRaw1, RawMin, RawMax, -3000, 3000);
+      long Scaled1[3];
+        for(int i=0; i<=2; i++){
+          Scaled1[i] = map(Raw1[i], RawMin, RawMax, -3000, 3000);
+        }
+    
+      // converting accelerations to fractional Gs
+      float xAccel1 = xScaled1 / 1000.0;
+      float yAccel1 = yScaled1 / 1000.0;
+      float zAccel1 = zScaled1 / 1000.0;
+      float Accel1[3];
+      for(int i=0; i<=2; i++){
+        Accel1[i] = Scaled1[i] / 1000.0;
+      }
+      
       Serial.print(force1);
       Serial.print(",");
       Serial.println(force2);
+      Serial.print(xAccel1);
+      Serial.print(",");
+      Serial.print(yAccel1);
+      Serial.print(",");
+      Serial.println(zAccel1);
       newDataReady = 0;
       t = millis();
     }
   }
 
+  //////////////////////////
+  // TARE OPERATION
   // receive command from serial terminal, send 't' to initiate tare operation:
   if (Serial.available() > 0) {
     char inByte = Serial.read();
@@ -94,13 +150,24 @@ void loop() {
       LoadCell_2.tareNoDelay();
     }
   }
-
   //check if last tare operation is complete
   if (LoadCell_1.getTareStatus() == true) {
-    //Serial.println("Tare load cell 1 complete");
+    Serial.println("Tare load cell 1 complete");
   }
   if (LoadCell_2.getTareStatus() == true) {
-    //Serial.println("Tare load cell 2 complete");
+    Serial.println("Tare load cell 2 complete");
   }
+}
 
+
+// takes samples and return the average
+int ReadAxis(int axisPin)
+{
+  long reading = 0;
+  analogRead(axisPin);
+  delay(1);
+  for (int i = 0; i < sampleSize; i++){
+    reading += analogRead(axisPin);
+  }
+  return reading/sampleSize;
 }
